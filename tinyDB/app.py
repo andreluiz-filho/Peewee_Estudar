dc5eec7e1011e810bda6e7132aed9aae0d1a5cf6
andreluiz-filho <andreluiz.melofilho@gmail.com>
2022-06-03 23:19:19 -0300

    .

--- tinyDB/app.py
+++ tinyDB/app.py
@@ -20,11 +20,26 @@ def criar_usuario(login, senha):
 
 criar_usuario('admin1', '123')
 
+def fazer_login(login, senha):
+	usuarios = db.table('usuarios')
+	u = usuarios.search(Query().login == login)
+	if len(u) == 1:
+		if u[0]['senha'] == senha:
+			print("Logado com Sucesso!")
+		else:
+			print("Usuário ou Senha Incorretos")	
+
+	elif u == []:
+		print("Usuário ou Senha Incorretos")
+
+
+fazer_login('admin', '123')
+
 """
 #Cria a tabela empresa
 empresas = db.table('empresas')
 """
 
-u = db.table('usuarios')
-print(u.all())
+#u = db.table('usuarios')
+#print(u.all())
 #print(u.search(Query().login == login))
